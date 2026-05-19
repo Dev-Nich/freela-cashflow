05c1645ff81ed9ece5af3370bf5b08a4f36f64ce
Dev-Nich <Dev-Nich@users.noreply.github.com>
2026-05-18 22:38:45 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/test/java/com/nicholas/freelacashflow/FreelaCashFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicholas.freelacashflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelaCashFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
